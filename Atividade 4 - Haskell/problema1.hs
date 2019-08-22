f581d94ef6280e1a0f643246ee5cd6e34cbb775a
milkfrog <contin.marcelo@gmail.com>
2019-08-22 14:08:50 -0700

    dsd

--- Atividade 4 - Haskell/problema1.hs
+++
@@ -1,8 +0,0 @@
-main = do
-	putStrLn "Digite o valor de X: "
-	xStr <- getLine
-	let x = (read xStr :: Float)
-	putStrLn "Digite o valor de y: "
-	yStr <- getLine
-	let y = (read yStr :: Float)
-	print (x**y)
