7601161354ce934fb505f883672ed80bc30b6e07
milkfrog <contin.marcelo@gmail.com>
2019-08-23 11:51:02 -0700

    deletando arquivos desnecessarios

--- Atividade_4_Haskell/first.hs
+++
@@ -1,4 +0,0 @@
-main = do
-    putStrLn "Informe sua idade: "
-    idade <- getLine
-    putStrLn ("Sua idade é " ++ idade)
--- Atividade_4_Haskell/second.hs
+++
@@ -1,7 +0,0 @@
-main = do
-	print 5.75
-	print 6
-	print True
-	print "Floripa"
-	print [1,2,3]
-	print ["a", "b", "c"]
