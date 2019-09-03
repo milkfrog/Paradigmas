143ed660e6630defddec456423e77dbe528e4f4c
milkfrog <contin.marcelo@gmail.com>
2019-09-03 11:09:43 -0700

    problema1

--- Atividade_6_Haskell/problema1.hs
+++ Atividade_6_Haskell/problema1.hs
@@ -8,25 +8,24 @@ aluno 1 = ("Marcelo", "Paradigmas", 10, 10, 10)
 aluno 2 = ("Jose", "ED", 5, 4, 7)
 aluno 3 = ("Chico", "Calculo 1", 1.5, 2.7, 3.4)
 
-fazMedia :: Float -> Float -> Float -> Float
-fazMedia a b c = ((a + b + c)/3)
-
-getNota1 :: Aluno -> Nota
-getNota1 (_,_,n,_,_) = n
-
-getNota2 :: Aluno -> Nota
-getNota2 (_,_,_,n,_) = n
-
-getNota3 :: Aluno -> Nota
-getNota3 (_,_,_,_,n) = n
+getNotas :: Aluno -> Nota
+getNotas (_,_,a,b,c) = a + b + c
 
 notaFinal :: Int -> Float
-notaFinal x = (fazMedia getNota1(x) getNota2(x) getNota3(x))
+notaFinal x = (getNotas (aluno x))/3
 
 getNome :: Aluno -> Nome
 getNome (n,_,_,_,_) = n
 
+somaTodos :: Int -> Float
+somaTodos quantidade
+	| (quantidade == 0) = 0
+	| otherwise = (notaFinal quantidade) + (somaTodos(quantidade - 1))
+
+notaFinalTodos :: Int -> Float
+notaFinalTodos quantidade = ((somaTodos quantidade) / fromIntegral(quantidade))
+
 main = do
 	print(getNome(aluno 1))
-	print((getNota1(aluno 2) + getNota2(aluno 2) + getNota3(aluno 2))/3)
 	print(notaFinal 2)
+	print(notaFinalTodos 3)
